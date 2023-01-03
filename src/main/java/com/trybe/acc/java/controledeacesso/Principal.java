6b0ce1e2b8f2ca9b8260f4bb37e22d975cb28928
Thayssa Bernardo <thayssabernardo@gmail.com>
2023-01-03 18:23:05 -0300

    Arrumando duplicaçao do menu, e formantando projeto

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,53 +1,53 @@
 package com.trybe.acc.java.controledeacesso;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Principal {
   public static Scanner scanner = new Scanner(System.in);
-  public static void main(String[] args) {
-	  
-	  ArrayList<Short> list = new ArrayList<Short>();
-	  
-	  short acessoEscolhido;
 
-	  do {		  
-		  acessoEscolhido = scanMenu();
-		  if(acessoEscolhido == 1) {
-			  System.out.println("Entre com a idade:");
-			  
-			  String idade = scanner.next();
-			  short idadeDaPessoa = Short.parseShort(idade);
-			  
-			  list.add(idadeDaPessoa);
-			  
-			  if(idadeDaPessoa < 18) {
-				  System.out.println("Pessoa cliente menor de idade, catraca liberada!");
-				  scanMenu();
-			  } else if (idadeDaPessoa >= 18 && idadeDaPessoa <= 49) {
-				  System.out.println("Pessoa adulta, catraca liberada!");
-				  scanMenu();
-			  } else {
-				  System.out.println("Pessoa adulta a partir de 50, catraca liberada!");
-				  scanMenu();
-			  }
-		  }
-	  } while(acessoEscolhido == 2);
-	  
+  public static void main(String[] args) {
+
+    ArrayList<Short> list = new ArrayList<Short>();
+
+    short acessoEscolhido;
+    acessoEscolhido = scanMenu();
+
+    do {
+      System.out.println("Entre com a idade:");
+
+      String idade = scanner.next();
+      short idadeDaPessoa = Short.parseShort(idade);
+
+      list.add(idadeDaPessoa);
+
+      if (idadeDaPessoa < 18) {
+        System.out.println("Pessoa cliente menor de idade, catraca liberada!");
+        scanMenu();
+      } else if (idadeDaPessoa >= 18 && idadeDaPessoa <= 49) {
+        System.out.println("Pessoa adulta, catraca liberada!");
+        scanMenu();
+      } else {
+        System.out.println("Pessoa adulta a partir de 50, catraca liberada!");
+        scanMenu();
+      }
+    } while (acessoEscolhido == 1);
+
   }
-  
+
   public static void showMenu() {
-	  System.out.println("Entre com o número correspondente à opção desejada:");
-	  System.out.println("1 - Acessar o estabelecimento");
-	  System.out.println("2 - Finalizar sistema e mostrar relatório");
+    System.out.println("Entre com o número correspondente à opção desejada:");
+    System.out.println("1 - Acessar o estabelecimento");
+    System.out.println("2 - Finalizar sistema e mostrar relatório");
   }
-  
+
   public static short scanMenu() {
-	  showMenu();
-	  
-	  String acesso = scanner.next();
-	  
-	  short acessoConfirmado = Short.parseShort(acesso);
-	  
-	  return acessoConfirmado;
+    showMenu();
+
+    String acesso = scanner.next();
+
+    short acessoConfirmado = Short.parseShort(acesso);
+
+    return acessoConfirmado;
   }
 }
