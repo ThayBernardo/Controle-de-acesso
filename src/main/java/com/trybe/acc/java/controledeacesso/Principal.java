8ce12651e703c2d9e898e844d28385415f58d386
Thayssa Bernardo <thayssabernardo@gmail.com>
2023-01-04 14:48:26 -0300

    Funcionalidade de opçoes

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,38 +1,43 @@
 package com.trybe.acc.java.controledeacesso;
 
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Principal {
   public static Scanner scanner = new Scanner(System.in);
 
   public static void main(String[] args) {
 
-    ArrayList<Short> list = new ArrayList<Short>();
+    ArrayList<String> list = new ArrayList<String>();
 
     short acessoEscolhido;
-    acessoEscolhido = scanMenu();
 
     do {
-      System.out.println("Entre com a idade:");
-
-      String idade = scanner.next();
-      short idadeDaPessoa = Short.parseShort(idade);
-
-      list.add(idadeDaPessoa);
-
-      if (idadeDaPessoa < 18) {
-        System.out.println("Pessoa cliente menor de idade, catraca liberada!");
-        scanMenu();
-      } else if (idadeDaPessoa >= 18 && idadeDaPessoa <= 49) {
-        System.out.println("Pessoa adulta, catraca liberada!");
-        scanMenu();
-      } else {
-        System.out.println("Pessoa adulta a partir de 50, catraca liberada!");
-        scanMenu();
+      acessoEscolhido = scanMenu();
+      
+      if(acessoEscolhido != 1 && acessoEscolhido != 2) {
+        System.out.println("Entre com uma opção válida");
+        continue;
       }
-    } while (acessoEscolhido == 1);
 
+      if (acessoEscolhido == 1) {
+        System.out.println("Entre com a idade:");
+
+        String idade = scanner.next();
+        short idadeDaPessoa = Short.parseShort(idade);
+
+        if (idadeDaPessoa < 18) {
+          System.out.println("Pessoa cliente menor de idade, catraca liberada!");
+        } else if (idadeDaPessoa >= 18 && idadeDaPessoa <= 49) {
+          System.out.println("Pessoa adulta, catraca liberada!");
+        } else {
+          System.out.println("Pessoa adulta a partir de 50, catraca liberada!");
+        }
+      } else {
+        System.out.println("Finalizado");
+      }
+    } while (acessoEscolhido != 2);
+    scanner.close();
   }
 
   public static void showMenu() {
